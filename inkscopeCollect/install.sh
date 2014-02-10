2820e8690533d536a6f38968acb0b03be985fda9
Eric <eric.mourgaya@gmail.com>
2014-02-10 16:25:47 +0100

    a small dispatch  for install

---
+++ inkscopeCollect/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ # create  inskcope location conf and scripts
+
+INKPATHETC='/opt/inkscope/etc'
+INKPATHBIN='/opt/inkscope/bin'
+mkdir -pv $INKPATHETC $INKPATHBIN
+
+mv cephprobe.conf $INKPATHETC
+mv cephprobe.py $INKPATHBIN
+mv sysprobe.conf $INKPATHETC
+mv sysprobe.py  $INKPATHBIN
