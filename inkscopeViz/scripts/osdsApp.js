7384aa43032ade1c27ac84fc12042790b4d72c35
A-Dechorgnat <alain.dechorgnat@orange.com>
2015-04-16 17:54:03 +0200

    add status history graph for OSD

--- inkscopeViz/scripts/osdsApp.js
+++ inkscopeViz/scripts/osdsApp.js
@@ -66,13 +66,15 @@ OsdsApp.controller("OsdsCtrl", function ($rootScope, $scope, $http, $location ,$
 
                 // search for selected osd, first one if none
                 if ($scope.selectedOsd+"" == "undefined"){
-                    $scope.osd = data[0];
-                    $scope.selectedOsd = data[0].id;
+                    $scope.osdSelect(data[0]);
+                    //$scope.osd = data[0];
+                    //$scope.selectedOsd = data[0].id;
                 }
                 else{
                     for ( var i=0; i<data.length;i++){
                         if (data[i].id == $scope.selectedOsd){
-                            $scope.osd = data[i];
+                            //$scope.osd = data[i];
+                            $scope.osdSelect(data[i]);
                             break;
                         }
                     }
@@ -123,6 +125,91 @@ OsdsApp.controller("OsdsCtrl", function ($rootScope, $scope, $http, $location ,$
     $scope.osdSelect = function (osd) {
         $scope.osd = osd;
         $scope.selectedOsd = osd.node._id;
+
+        var requestData = '{"$select" : {"osd.$id":'+$scope.selectedOsd+'},"$template" : {"timestamp":1, "up":1, "in":1}}';
+        $http({method: "post", url:  inkscopeCtrlURL + $rootScope.fsid+"/osdstat", data: requestData}).
+            success(function(data,status){
+                addStatGraph(data);
+            }).
+            error(function (data, status, headers) {
+                $scope.stats=[];
+            }
+        );
+
+        function addStatGraph(data){
+            //Line chart data should be sent as an array of series objects.
+            var osd_up = [],osd_in = [];
+            var last_up = -1;
+            var last_in = -1;
+            var lastElement={};
+            data.sort( function( a, b ) { return a.timestamp - b.timestamp });
+            data.forEach(function(e,i,tab){
+                if ((last_up!= e.up)||(last_in!= e.in)) {
+                    //console.log(e.timestamp+" "+ e.up+" "+ e.in);
+                    if (last_up!=-1){
+                        osd_up.push({x: e.timestamp-1, y: last_up?3:2});
+                        osd_in.push({x: e.timestamp-1, y: last_in?1:0});
+                    }
+                    osd_up.push({x: e.timestamp, y: e.up?3:2});
+                    osd_in.push({x: e.timestamp, y: e.in?1:0});
+                    last_up = e.up;
+                    last_in = e.in;
+                }
+                lastElement = e;
+            });
+            osd_up.push({x: lastElement.timestamp, y: lastElement.up?3:2});
+            osd_in.push({x: lastElement.timestamp, y: lastElement.in?1:0});
+            data =  [
+                {
+                    values: osd_up,      //values - represents the array of {x,y} data points
+                    key: 'up/down ', //key  - the name of the series.
+                    color: '#3030ff'  //color - optional: choose your own line color.
+                },
+                {
+                    values: osd_in,
+                    key: 'in/out',
+                    color: '#ff7f0e'
+                }
+            ];
+
+            nv.addGraph(function() {
+                var chart = nv.models.lineChart()
+                        .margin({left: 35,right:40})  //Adjust chart margins to give the x-axis some breathing room.
+                        .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
+                        .transitionDuration(350)  //how fast do you want the lines to transition?
+                        .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+                        .showYAxis(true)        //Show the y-axis
+                        .showXAxis(true)        //Show the x-axis
+                        .forceY([0,3])
+                    ;
+
+                chart.xAxis     //Chart x-axis settings
+                    .tickFormat(function(d) { return d3.time.format('%X')(new Date(d)) });
+
+                chart.xScale(d3.time.scale()); //fixes misalignment of timescale with line graph
+
+                chart.yAxis     //Chart y-axis settings
+                    .axisLabel('state')
+                    .tickFormat(function(d) {
+                            if (d==0) return "out";
+                            if (d==1) return "in";
+                            if (d==2) return "down";
+                            if (d==3) return "up";
+                        });
+
+                d3.select('#osdStatGraph svg')    //Select the <svg> element you want to render the chart in.
+                    .datum(data)         //Populate the <svg> element with chart data...
+                    .call(chart);          //Finally, render the chart!
+
+                //Update the chart when window resizes.
+                nv.utils.windowResize(function() { chart.update() });
+                return chart;
+            });
+
+        }
+
+
+
     }
 
     $scope.osdIn = function (osd) {
