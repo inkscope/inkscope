2a0b8dbf39b3e1652ce404df9c50af23d3feaccd
A-Dechorgnat <alain.dechorgnat@orange.com>
2016-02-16 09:19:45 +0100

    fix login issue when ceph is not intalled on Inkscope server

--- inkscopeCtrl/inkscopeCtrlcore.py
+++ inkscopeCtrl/inkscopeCtrlcore.py
@@ -14,7 +14,7 @@ from hashlib import md5
 from bson.json_util import dumps
 from InkscopeError import InkscopeError
 
-version = "1.3.1"
+version = "1.3.2-alpha"
 
 app = Flask(__name__)
 app.secret_key = "Mon Nov 30 17:20:29 2015"
@@ -102,17 +102,19 @@ class User(UserMixin):
 
 
 def get_ceph_version():
-    args = ['ceph',
-            '--version']
-    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = p.communicate()
-    if p.returncode != 0:
+    try:
+        args = ['ceph',
+                '--version']
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = p.communicate()
+        if p.returncode != 0:
+            return "not found"
+        ceph_version = re.search('[0-9]*\.[0-9]*\.[0-9]*', output)
+        if ceph_version:
+            return ceph_version.group(0)
         return "not found"
-    ceph_version = re.search('[0-9]*\.[0-9]*\.[0-9]*', output)
-    if ceph_version:
-        return ceph_version.group(0)
-    return "not found"
-
+    except:
+        return '0.0.0 (could not be found on inkscope server - Please consider to install Ceph on it)'
 
 def get_ceph_version_name(ceph_version):
     major, minor, revision = ceph_version.split(".")
@@ -125,21 +127,23 @@ def get_ceph_version_name(ceph_version):
         if minor == 94:
             return 'Hammer'
         if minor > 87:
-            return 'Pre-Hammer'
+            return 'Hammer (pre-version)'
         if minor == 87:
             return 'Giant'
         if minor > 80:
-            return 'Pre-Giant'
+            return 'Giant (pre-version)'
         if minor == 80:
             return 'Firefly'
         if minor > 72:
-            return 'Pre-Firefly'
+            return 'Firefly (pre-version)'
         if minor == 72:
             return 'Emperor'
         if minor > 67:
-            return 'Pre-Emperor'
+            return 'Emperor (pre-version)'
         if minor == 67:
             return 'Dumpling'
+        if minor == 0:
+            return 'Unavailable'
         return 'Really too old'
 
 
